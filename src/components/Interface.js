4d7f3b59fda6a77e5e8da76951959ae1b705118e
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-08-02 09:53:42 -0300

    alteraçoes

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -1,44 +1,16 @@
 import React, { useState } from 'react';
 import { Text, TextInput } from 'react-native';
 import Button from './Button';
-import Display from './Display'
 export default props => {
    const[altura, setAltura] = useState('')
    const [peso, setPeso] = useState('')
-   const imc = peso / (altura * altura)
+   
 
 let imcfaixa = 0
 let classificacao = null
  
 // em breve transformar essa função em switch case
-if(imc < 16.9){
-    imcfaixa=1
-    classificacao="Muito abaixo do peso"     
-  }
-  else if (imc >= 17 && imc <= 18.4){
-      imcfaixa=2
-      classificacao="Abaixo do peso"
-  }
-  else if(imc >= 18.5 && imc <= 24.9){
-    imcfaixa=3
-    classificacao="Peso Normal"
-  }
-  else if (imc >= 25 && imc <= 29.9){
-    imcfaixa=4
-    classificacao="Acima do Peso"
-  }
-  else if (imc >= 30 && imc <= 34.9){
-    imcfaixa=5
-    classificacao="Obesidade Grau I"
-  }
-  else if (imc >= 35 && imc <= 40){
-    imcfaixa=6
-    classificacao="Obesidade Grau II"
-  }
-  else {
-    imcfaixa=7
-    classificacao="Obesidade Grau III"
-  }
+
   
    
    
@@ -62,7 +34,7 @@ if(imc < 16.9){
         <Text>Peso: {peso} </Text>
       
         <Button />
-        <Display imc={imc} faixa={classificacao}/>
+        
     </>
   );
 };

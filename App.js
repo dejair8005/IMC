344336d8902d6e32af7c20619c4c324ef5cd75ac
dejair8005 <68168713+dejair8005@users.noreply.github.com>
2023-08-25 12:04:11 -0300

    updates add navigation stack

--- App.js
+++ App.js
@@ -1,105 +1,83 @@
-import * as React from 'react';
-import { Component, useState } from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Imput from './src/componentes/Imput';
+import Estilos from './src/componentes/Estilos';
 
+// Create a stack navigator
+const Stack = createNativeStackNavigator();
 
-import Imput from './src/componentes/Imput'
-import Estilos from './src/componentes/Estilos'
-
-
-
+// HomeScreen component
 function HomeScreen() {
+  const navigation = useNavigation(); // Get the navigation prop
+
+  // State variables
+  const [altura, setAltura] = useState('0');
+  const [peso, setPeso] = useState('0');
+
+  // Function to calculate IMC
+  const calcimc = (a, p) => {
+    const calculatedimc = p / (a * a);
+    navigation.navigate('Resultado', { imc: calculatedimc.toFixed(2) }); // Pass imc as a route parameter
+  };
+
+  let txta = "Digite sua altura ex: 1.80";
+  let txtp = "Digite seu peso ex 63.0";
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={Estilos.conteiner}>
+      <View style={Estilos.Display1}>
+        {/* Using the Imput component */}
+        <Imput txt={txta} valor={(n) => { setAltura(n) }} />
+        <Imput txt={txtp} valor={(n) => { setPeso(n) }} />
+
+        {/* Calculate IMC and navigate to Resultado screen */}
+        <Button
+          title="Calcular"
+          onPress={() => {
+            calcimc(parseFloat(altura), parseFloat(peso));
+          }}
+        />
+      </View>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
+// Resultado component
+function Resultado({ route }) {
+  const { imc } = route.params; // Access the imc parameter from route.params
+  
 
-function App() {
+  return (
+    <View style={Estilos.conteiner}>
+      <View style={Estilos.Display2}>
+        {imc !== 0 ? (
+          <View>
+            <Text>Resultado</Text>
+            <Text>Seu imc é: {imc}</Text>
+          </View>
+        ) : (
+          <View>
+            <Text>Aguardando</Text> {/* Display "Aguardando" when imc is 0 */}
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
+// Main App component
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Calculadora de IMC" component={HomeScreen} />
+        <Stack.Screen
+          name="Resultado"
+          component={Resultado}
+          options={{ title: 'Resultado do IMC' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
-
-
-
-
-
-
-
-
-/*export default function App() {
-
-  let txta = "Digite sua altura ex: 1.80"
-  let txtp = "Digite seu peso ex 63.0"
-
-  
-  const [altura, setAltura]=useState('0');
-  const [peso, setPeso]=useState('0');
-
-
-  
-  const [imc, setImc]=useState (0);
-  const calcimc = (a,p)=>{
-    const calculatedimc = p / (a * a)
-    setImc(calculatedimc.toFixed(2))    
-  };
-  
-
-
-
-  return (
-    <NavigationContainer>
-      <View style={Estilos.conteiner}>
-        <View style={Estilos.Display1}>
-            <Text style={Estilos.title}>Calculadora de IMC</Text>
-
-
-            <Imput txt={txta} valor={(n)=>{setAltura(n)}}/>
-            <Imput txt={txtp} valor={(n)=>{setPeso(n)}}/>
-            
-            <Button
-            title="Calcular"
-            onPress={() => {
-              calcimc(parseFloat(altura), parseFloat(peso));
-            }}
-            
-            />  
-        </View>
-
-
-        <View style={Estilos.Display2}>
-        {imc !== 0?    
-        <View >
-          <Text>Resuldado</Text>
-          
-          <Text>
-            Seu imc é: {imc}
-            
-          </Text>
-          
-        </View>
-        : false}
-
-        </View>   
-
-      </View>
-    </NavigationContainer>
-    
-  );
-}
-*/
-
-
-
---
+++ src/componentes/Resultado.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {View, Text} from "react-native"
+export default function Resultado() {
+    <View style={Estilos.conteiner}>
+      <Text>Tela de Resultado</Text>
+      
+    </View>
+  }
